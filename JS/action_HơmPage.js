8f5eeeca73f44fb67cc6cdbbdf4982ce6e9e34f4
unknown <ldmhieudev@gmail.com>
2023-03-02 00:06:58 +0700

    add page log in

--- JS/action_HơmPage.js
+++ JS/action_HơmPage.js
@@ -1,21 +1,27 @@
 var image = new  Array()
-image[0] = '../IMAGE/Banner/1.jpg'
-image[1] = '../IMAGE/Banner/2.jpg'
-image[2] = '../IMAGE/Banner/3.jpg'
 
+
+image[0] = '../IMAGE/laptoptuutruong_800x450.jpg'
+image[1] = '../IMAGE/news_Sale.jpg'
+image[2] = '../IMAGE/laptop-asus-800-450_800x450.png'
 for(var i = 0;i<image.length;i++){
     console.log(image[i]);
 }
 
+console.log(image.length);
+
 var x = 0;
 
 function changeBanner(){
+    console.log(x);
     document.getElementById('Sale').src=image[x];
-    x++;
-    if(x==image.length-1)
+    if(x>=image.length-1)
         x = 0;
-
-        setTimeout('changeBanner()',3000);
-
+    else
+        x++;
+    
+    setTimeout('changeBanner()',3000);
 }
 setTimeout('changeBanner()',3000);
+
+
---
+++ JS/test.js
@@ -0,0 +1,10 @@
+var btn = document.getElementsByClassName('box1')
+function he(){
+    console.log("he;;p");
+}
+
+// var html = document.getElementById("box2").innerHTML;
+// html = parserInt(html)
+// console.log(html);
+
+ document.getElementsByClassName('box2').innerHTML = "hi";
