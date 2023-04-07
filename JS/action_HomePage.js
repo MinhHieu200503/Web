048e9169362de07b4fe36be2cb52e51923a54e38
unknown <ldmhieudev@gmail.com>
2023-04-07 09:39:35 +0700

    layout homepage

--- JS/action_HomePage.js
+++ JS/action_HomePage.js
@@ -155,3 +155,21 @@ setTimeout('changeBanner()',3000);
 
     let btn_register = document.getElementById("formBuyNow_register");
     btn_register.addEventListener('click',checkRegex);
+
+    function isShowNav(){
+        let sts =  document.querySelector('.nav');
+        if(sts.style.display == 'block')
+            return true;
+        else
+            return false;
+    }
+
+    let shownav = document.getElementById('respon__menu');
+    shownav.addEventListener('click',function(){
+        let sts =  document.querySelector('.nav');
+        console.log(isShowNav());
+        if(isShowNav())
+            sts.style.display = 'none';
+        else
+            sts.style.display = 'block';
+    })
